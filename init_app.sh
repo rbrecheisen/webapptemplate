7e52342a67266e7e33489e1d25d2c61b24f50aa9
Ralph Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2023-04-08 08:01:14 +0200

    saving work

---
+++ init_app.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd webapp/app/migrations
+rm -r *
+touch __init__.py
+
+cd ../..
+
+rm db.sqlite3
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py load_courses
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
+echo "Successfully created super user \"admin\""
+
+python manage.py runserver
