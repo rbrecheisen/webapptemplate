6745e838bc1652417828d488bb38c49898131d58
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2023-04-11 11:03:29 +0200

    added post_delete signal to models.py

--- template/webapp/app/models.py
+++ template/webapp/app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.dispatch import receiver
 
 
 class ObjectModel(models.Model):
@@ -13,3 +14,11 @@ class ChildObjectModel(models.Model):
     obj = models.ForeignKey('ObjectModel', on_delete=models.CASCADE)
     def __str__(self):
         return f'ChildObjectModel("{self.name}", ObjectModel("{self.obj.name}"))'
+
+
+@receiver(models.signals.post_delete, sender=ChildObjectModel)
+def child_object_post_delete(sender, instance, **kwargs):
+    # import os
+    # if os.path.isfile(instance.some_file):
+    #     os.remove(instance.some_file)
+    pass
